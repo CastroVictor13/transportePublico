7f0b740f23a3f33397a1b8941298baff2e3dec9f
victor <victorcaalves@gmail.com>
2024-12-06 20:05:25 -0300

    feat: initial commit

--- src/main/java/com/transporte/controller/LinhaController.java
+++ src/main/java/com/transporte/controller/LinhaController.java
@@ -9,29 +9,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/linha")
 public class LinhaController {
 
     @Autowired
-    private LinhaRepository dadoRepository;
+    private LinhaRepository linhaRepository;
 
     @GetMapping("/consultar")
     public String consultar(@RequestParam String pesquisa, Model model) {
-        List<Linha> dados = new ArrayList<>();
-
-        try {
-            List<Linha> linhas = dadoRepository.findByNome(pesquisa);
-            dados.addAll(linhas);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+        List<Linha> dados = linhaRepository.findByNome(pesquisa);
         model.addAttribute("dados", dados);
-        return "listagem";
+        return "consulta";
     }
 }
